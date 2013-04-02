09f77186a127cd0c882a73996d9bb809c06990f4
Andrea Reginato <andrea.reginato@gmail.com>
2013-04-02 13:09:40 +0200

    Completed test suite for type dashboard

--- test/spec/controllers/dashboard-controller.js
+++ test/spec/controllers/dashboard-controller.js
@@ -1,42 +1,42 @@
-//'use strict';
+'use strict';
 
-//describe('dashboard home', function() {
+describe('dashboard home', function() {
 
-  //var page  = '/mocks/index.html';
+  var page  = '/mocks/index.html';
 
-  //beforeEach(function() {
-    //browser().navigateTo(page);
-  //});
+  beforeEach(function() {
+    browser().navigateTo(page);
+  });
 
-  //it('shows the introduction page', function() {
-    //expect(element('.home .title').text()).toMatch('Lelylan Types');
-  //});
+  it('shows the introduction page', function() {
+    expect(element('.home .title').text()).toMatch('Lelylan Types');
+  });
 
-  //describe('when clicks the search button', function() {
+  describe('when clicks the search button', function() {
 
-    //beforeEach(function() { element('.home .search').click(); });
+    beforeEach(function() { element('.home .search').click(); });
 
-    //it('shows the popular page', function() {
-      //expect(element('.sidebar-nav .active').text()).toMatch('Popular');
-    //});
-  //});
+    it('shows the popular page', function() {
+      expect(element('.sidebar-nav .active').text()).toMatch('Popular');
+    });
+  });
 
-  //describe('when clicks the create button', function() {
+  describe('when clicks the create button', function() {
 
-    //beforeEach(function() { element('.home .create').click(); });
+    beforeEach(function() { element('.home .create').click(); });
 
-    //it('shows the create page', function() {
-      //expect(element('.sidebar-nav .active').text()).toMatch('Create type');
-    //});
-  //});
+    it('shows the create page', function() {
+      expect(element('.sidebar-nav .active').text()).toMatch('Create type');
+    });
+  });
 
-  //describe('when clicks the home button', function() {
+  describe('when clicks the home button', function() {
 
-    //beforeEach(function() { element('.home .create').click(); });
-    //beforeEach(function() { element('.navbar .home').click(); });
+    beforeEach(function() { element('.home .create').click(); });
+    beforeEach(function() { element('.navbar .home').click(); });
 
-    //it('removes the highlight from any menu item', function() {
-      //expect(repeater('.sidebar-nav li.active').count()).toBe(0);
-    //});
-  //});
-//});
+    it('removes the highlight from any menu item', function() {
+      expect(repeater('.sidebar-nav li.active').count()).toBe(0);
+    });
+  });
+});
