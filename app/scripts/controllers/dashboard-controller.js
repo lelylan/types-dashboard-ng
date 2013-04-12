6bab8e050aeded5e7adc3c5c88cc5a6258fe3662
Andrea Reginato <andrea.reginato@gmail.com>
2013-04-12 18:02:26 +0200

    Updated spinner visualization

--- app/scripts/controllers/dashboard-controller.js
+++ app/scripts/controllers/dashboard-controller.js
@@ -8,21 +8,16 @@ function DashboardCtrl($scope, $rootScope, $http) {
     scope:    '<scope>',
     state:    '<state>'
   };
-
-  var url = 'http://types.lelylan.com/types/public';
-  $http({ method: 'GET', url: url }).success(function() {console.log("YUO")});
 };
 
 DashboardCtrl.$inject = ['$scope', '$rootScope', '$http'];
 
-
-  var cl = new CanvasLoader('lelylan-request-loading');
-  cl.setColor('#239cbb');
-  cl.setDiameter(20);
-  cl.setDensity(70);
-  cl.setRange(0.7);
-  cl.setSpeed(2);
-  cl.setFPS(35);
-  cl.show();
-
-
+var cl = new CanvasLoader('lelylan-request-loading');
+cl.setColor('#239cbb');
+cl.setShape('spiral');
+cl.setDiameter(20);
+cl.setDensity(70);
+cl.setRange(0.7);
+cl.setSpeed(2);
+cl.setFPS(35);
+cl.show();
