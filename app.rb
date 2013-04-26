35dd8c43082367bcdd28f1410d7c76881905d2b1
Andrea Reginato <andrea.reginato@gmail.com>
2013-04-26 12:52:28 +0200

    Refactoring static service of page

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-  get '/*', provides: 'html' do
-    File.new('app/index.html').readlines
-  end
-
   set :public_folder, File.dirname(__FILE__) + '/app'
+
+  get '/*', provides: 'html' do
+    send_file File.join(settings.public_folder, 'index.html')
+  end
 end
 
