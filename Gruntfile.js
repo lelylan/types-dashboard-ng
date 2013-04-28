e676e40a4c8a40f3efed45a885d8630202fa33d4
Andrea Reginato <andrea.reginato@gmail.com>
2013-04-28 19:43:18 +0200

    Improved the development structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -259,7 +259,8 @@ module.exports = function (grunt) {
             '.htaccess',
             'components/**/*',
             'images/{,*/}*.{gif,webp}',
-            'styles/fonts/*'
+            'styles/fonts/*',
+            'views/**/*'
           ]
         }]
       }
