d497ddf28256db447e48dcae05af0d5b935128aa
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-10 11:27:23 +0200

    Added popular types

--- app/scripts/controllers/popular-controller.js
+++ app/scripts/controllers/popular-controller.js
@@ -8,22 +8,3 @@ function PopularCtrl($scope, $rootScope) {
 }
 
 PopularCtrl.$inject = ['$scope', '$rootScope'];
-
-var popularTypesList =
-  [{
-    uri: 'http://api.lelylan.com/types/1',
-    id: '1',
-    name: 'Basic Light',
-    description: 'The **Basic Light** type represents the most basic light structure.',
-    categories: ['lights'],
-    created_at: '2012-09-01T15:01:22Z',
-    updated_at: '2012-09-01T15:01:22Z',
-  }, {
-    uri: 'http://api.lelylan.com/types/2',
-    id: '2',
-    name: 'Complex Light',
-    description: 'The **Comples Light** type represents the most complete light structure.',
-    categories: ['lights'],
-    created_at: '2012-09-01T15:01:22Z',
-    updated_at: '2012-09-01T15:01:22Z',
-  }];
